ebcf6b9b77e828b37fe9b9212fc8f2e8d11320d7
Caio Théodore <167098967+tcaio26@users.noreply.github.com>
2025-05-07 13:05:34 -0300

    definições e gerações de estruturas raiz e filhos

--- funções nodo.R
+++ funções nodo.R
@@ -2,35 +2,54 @@ amostra = sample(c(0,1),1000,T) #entradas
 sensib = 0.1
 alpha = 0.05
 
+Nmin = log(alpha, base = sensib)
 
 match = function(str, p){ 
   pos = stringr::str_locate_all(str, p)[[1]][, "start"]
   return(pos)
 } #funções
 
-
-amostra_string = paste(amostra, collapse='')
-n_min = ceiling(log(alpha, base = 1-sensib))
-n_smp = length(amostra)
-alphabet = unique(amostra)
-
-val = list(index = 1:n_smp, n = n_smp)
-
-init.raiz = function(val){
-  contexto = 'r'
-  nivel = 0
-  cont = c(sum(amostra[val$index]), val$n-sum(amostra[val$index]))
-  gamma = ifelse(cont[1]==0 || cont[2]==0, sensib^(val$n), 1)
-  filhos_logico = (gamma>=alpha && val$n>=n_min)
-  filhos = list()
-  val = val
-  return(list(contexto = contexto, nivel = nivel, cont = cont, 
-              gamma = gamma, filhos_logico = filhos_logico, 
-              filhos = filhos, val = val))
+calc_gamma = function(x, sensib){
+  if((x[1]!=0)&&(x[2]!=0)) return(1)
+  else return((1-sensib)^max(x))
 }
 
-raiz = init.raiz(val)
-
-init.nod = function(pai, simbolo){
+new_skel_node = function(sample, sensib,
+                         index = 1:length(sample), father, symbol){
+  stopifnot(is.numeric(index))
+  if(any(index>length(sample))) stop()
+  if(!missing(father) && missing(symbol)) stop()
   
+  s = sample[index]
+  counts = c(length(s)-sum(s), sum(s))
+  structure(
+    list(
+      counts = counts,
+      gamma = calc_gamma(counts, sensib)
+    ),
+    class = 'Skel Node',
+    context = ifelse(missing(father), 'r', 
+                     paste0(attr(father, 'context'),as.character(symbol))),
+    index = index
+  )
 }
+
+nodotest = new_skel_node(amostra, sensib)
+
+need_children = function(node, alpha, Nmin){
+  stopifnot(class(node)=="Skel Node")
+  
+  (node$gamma>=alpha && sum(node$counts)>=Nmin)
+}
+
+need_children(nodotest, alpha, Nmin)
+
+make_children = function(node, sample, symbol, ...){
+  i = attr(node, 'index')
+  s = i[which(sample[i]==symbol)]+1
+  s = s[s<=max(i)]
+  
+  new_skel_node(sample, sensib, index = s, father = node, symbol = symbol)
+}
+
+moleque = make_children(nodotest, amostra, 0)
