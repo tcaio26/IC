2c54877dc75c78239820b60dd39b15bb1c55ee22
Caio Théodore <167098967+tcaio26@users.noreply.github.com>
2025-05-09 16:08:31 -0300

    função com nodos mais simples, checagem inicial da função principal

--- funções nodo.R
+++ funções nodo.R
@@ -53,3 +53,66 @@ make_children = function(node, sample, symbol, ...){
 }
 
 moleque = make_children(nodotest, amostra, 0)
+
+entre = function(x,a,b,inc.L=F,inc.U=F){
+  L = ifelse(inc.L,x>=a,x>a)
+  U = ifelse(inc.U,x<=b,x<b)
+  return(L&U)
+}
+
+generate_skeleton = function(sample, Nmin=59, sensibility, alpha){
+  use_nmin = !missing(Nmin)
+  #checks
+  if(!is.numeric(sample)&&!all(is.character(sample),length(sample)==1)){
+    stop("Sample must be a numeric vector or a string")
+  }
+  if(is.character(sample)) sample = as.numeric(unlist(strsplit(sample,'')))
+  if(!all(unique(sample)%in%c(0,1))) stop("Sample must be comprised of 0 and 1 values")
+  if(length(sample)<2) stop("Sample must be at least 2 symbols long")
+  
+  if(!(missing(alpha)||(is.double(alpha) && entre(alpha,0,1)))) stop("alpha must be a numeric value between 0 and 1")
+  if(!(missing(sensibility)||(is.double(sensibility) && entre(sensibility,0,0.5)))) stop("sensibility must be a numeric value between 0 and 0.5")
+  if(!(missing(Nmin)||(is.numeric(Nmin) && Nmin>0))) stop("Nmin must be a positive integer") 
+  
+  if(all(missing(sensibility), missing(alpha), missing(Nmin))){
+    warning("No tolerance arguments provided, proceeding with default Nmin = 59")
+    use_nmin=T
+  }
+  
+  if(xor(missing(sensibility),missing(alpha))){
+    warning("Provided only alpha or sensibility. Provide both or none. Proceeding with ", Nmin)
+    use_nmin=T
+  }
+  if(!missing(Nmin)&&length(sample)<=Nmin) stop("Sample length must be larger than Nmin, preferably a lot larger")
+  
+  #generating function
+  if(!use_nmin) Nmin = ceiling(log(alpha, 1-sensibility))
+  if(Nmin>=length(sample)) stop("Nmin too large for sample")
+  #tree = genskel(...)
+  
+  #output
+  
+  return(Nmin)
+}
+
+
+generate_skeleton('101010', Nmin=20)
+generate_skeleton(c(1,0,1,2))
+generate_skeleton(sample(c(0,1),1000,T),alpha=0.2)
+
+#função que faz mais sentido para ser recursiva:
+genskel = function(sample, index=1:length(sample), context){
+  s = sample[index]
+  n = length(s)
+  dom = ifelse(sum(s)%in%c(0,n), s[1], -1)
+  structure(
+    list(
+      index = index,
+      context = context,
+      n = n,
+      dom = dom
+    ),
+    class = 'Skel Node'
+  )
+}
+
