7aff794a9a5d412876f8f3bb27773205d7c534b7
tcaio26 <caiotheodore26@gmail.com>
2025-05-26 15:34:55 -0300

    FUNCIONANDO!! benchmarks

--- funcao_match.R
+++ funcao_match.R
@@ -1,16 +1,17 @@
 #APENAS PARA A FUNÇÃO QUE ENCONTRA AS POSIÇÕES X_i TQ CONTEXTO = W
 
 #amostra para validação
-# set.seed(247005)
-# a = sample(c(0,1),20,T)
-# a_txt = paste0(a, collapse='')
+set.seed(247005)
+a = sample(c(0,1),20,T)
+a_txt = paste0(a, collapse='')
 
 #o pacote stringi tem o parametro overlap = T, maravilhoso.
 
-match_str = function(str, p){ 
+match_str = function(str, p){
+  if(p == '') return(1:nchar(str))
   pos = stringi::stri_locate_all_fixed(str, p, overlap = T)[[1]][, "end"] +1 #retorna as posições logo após o contexto
   pos = pos[pos<=nchar(str)] #evita ultrapassar o index maximo
-  if(is.na(pos)) pos = 0
+  if(all(is.na(pos))) pos = 'no matches'
   return(pos)
 }
 # 
--- novas_func_skel.R
+++ novas_func_skel.R
@@ -21,36 +21,91 @@ genskel = function(node, sample_vec, sample_txt, Nmin, prob = F, debug = F){
   #resultado 0
   l = node$AddChild(paste0('0',node$name))
   w = paste0('0',node$context)
-  i = match(sample_txt, w)
-  n = ifelse(i==0, 0, length(i))
-  l$context = w
-  l$index = i
-  l$n = length(i)
-  l$dom = ifelse(sum(sample_vec[i])%in%c(0,n), sample_vec[i][1], -1)
-  if(prob) l$p = sum(sample_vec[i])/n
-  if(debug) print('OK')
-  if(length(i)>=Nmin){
-    genskel(l, sample_vec, sample_txt, Nmin, prob, debug)
+  if(debug) print(paste0('contexto ',w))
+  i = match_str(sample_txt, w)
+  if(any(i=='no matches')){
+    l$context = w
+    l$index = numeric(0)
+    l$n = 0
+    l$dom = -1
+    if(prob) l$p = numeric(0)
+    if(debug) print(paste0('nodo ',w,' vazio, OK'))
+  }
+  else{
+    n = length(i)
+    l$context = w
+    l$index = i
+    l$n = length(i)
+    l$dom = ifelse(sum(sample_vec[i])%in%c(0,n), sample_vec[i][1], -1)
+    if(prob) l$p = sum(sample_vec[i])/n
+    if(debug) print('OK')
+    if(n>=Nmin){
+      genskel(l, sample_vec, sample_txt, Nmin, prob, debug)
+    } 
   }
   
   
   #resultado 1
   r = node$AddChild(paste0('1',node$name))
   w = paste0('1',node$context)
-  i = match(sample_txt, w)
-  n = ifelse(i==0, 0, length(i))
-  r$context = w
-  r$index = i
-  r$n = length(i)
-  r$dom = ifelse(sum(sample_vec[i])%in%c(0,n), sample_vec[i][1], -1)
-  if(prob) r$p = sum(sample_vec[i])/n
-  if(debug) print('OK')
-  if(length(i)>=Nmin){
-    genskel(r, sample_vec, sample_txt, Nmin, prob, debug)
+  i = match_str(sample_txt, w)
+  if(any(i=='no matches')){
+    r$context = w
+    r$index = numeric(0)
+    r$n = 0
+    r$dom = -1
+    if(prob) r$p = numeric(0)
+    if(debug) print(paste0('nodo ',w,' vazio, OK'))
+  }
+  else{
+    n = length(i)
+    r$context = w
+    r$index = i
+    r$n = length(i)
+    r$dom = ifelse(sum(sample_vec[i])%in%c(0,n), sample_vec[i][1], -1)
+    if(prob) r$p = sum(sample_vec[i])/n
+    if(debug) print('OK')
+    if(n>=Nmin){
+      genskel(r, sample_vec, sample_txt, Nmin, prob, debug)
+    } 
   }
 }
 
-t = startskel('01000110', 1, T)
-df = ToDataFrameTree(t, 'context','n','p')
-df = df[df$n>0,]
-df[order(nchar(df$context)),]
+# t = startskel('01000110', 1, T, T)
+# df = ToDataFrameTree(t, 'context','n','p')
+# df = df[df$n>0,]
+# df[order(nchar(df$context)),]
+# 
+# library(rbenchmark)
+# 
+# amostra = readLines('amostra_skel_100k.txt')
+# 
+# amostra1000 = vec_to_string(string_to_vec(amostra)[1:1000])
+# amostra10000 = vec_to_string(string_to_vec(amostra)[1:10000])
+# 
+# benchmark(
+#   startskel(amostra1000, 100, T), #0.146, relativo 1
+#   startskel(amostra10000, 100, T), #2.495, relativo 17.089
+#   startskel(amostra, 100, T), #68.477, relativo 469.021
+#   replications = 10, order = c(1,2,3)
+# )
+# 
+# library(microbenchmark)
+# tam = numeric(100)
+# tempo = numeric(100)
+# amostra = sample(c(0,1),1e5,T)
+# s = sample(seq(1e3,1e5,1e3))
+# for(i in 1:100){
+#   tam[i] = s[i]
+#   a = vec_to_string(amostra[1:(s[i])])
+#   t = microbenchmark(startskel(a, 100, T), times = 5)
+#   tempo[i] = t$time[4]
+#   print(paste('rep',i,'tam',s[i], sep=' '))
+# }
+# df_bench = data.frame(tamanho_string = tam, tempo = tempo)
+# pdf('tempo_genskel_sem_prune.pdf')
+# ggplot2::ggplot(data = df_bench, ggplot2::aes(x=tam, y = tempo))+
+#   ggplot2::geom_point(color = 'darkred')+
+#   ggplot2::theme_bw()+
+#   ggplot2::labs(title='Tempo por tamanho de string, com Nmin=100, sem corte pós árvore')
+# dev.off()
