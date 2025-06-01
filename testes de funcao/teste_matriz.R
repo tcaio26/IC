b9f7a68015f4a7dd18d3b80d7791ae335e60ef3a
tcaio26 <caiotheodore26@gmail.com>
2025-06-01 17:23:25 -0300

    matriz de transição, acho que vai ser rápido

--- testes de funcao/teste_matriz.R
+++ testes de funcao/teste_matriz.R
@@ -1,2 +1,16 @@
 library(skeleton)
 
+t = readLines('amostra_skel_100k.txt')
+
+arv = startskel(t, 300, T)
+sculptskeleton(arv, 300)
+
+df = ToDataFrameTree(arv, 'context', 'p', 'dom', 'n', filterFun = isLeaf)
+
+df[,c('context','p')]
+
+context = df$context
+
+teststr = '00'
+maxcontxt = 4
+falta = maxcontxt - nchar(teststr)
