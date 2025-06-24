056ec71f632087312f7e65456d623d546c9fe067
tcaio26 <caiotheodore26@gmail.com>
2025-06-24 12:02:25 -0300

    testes de quais funções são necessarias

--- DELETEME.R
+++ DELETEME.R
@@ -1,11 +1,28 @@
-funções necessárias:
-  
-  generate_skeleton()
-  startskel()
-  genskel()
-  sculptskeleton()
-  shouldyoucut()
-  getMaxContext()
-  extractTransitions()
-  transitionsToMatrix()
-  matrixToTransitions()
+# funções necessárias:
+#   
+#   generate_skeleton()
+#   startskel()
+#   genskel()
+#   sculptskeleton()
+#   shouldyoucut()
+#   getMaxContext()
+#   extractTransitions()
+#   transitionsToMatrix()
+#   matrixToTransitions()
+library(skeleton)
+amostra = strsplit(readLines('amostra_chuva_4_100k.txt'),' ')[[1]]
+A = c('sol','nuvem','chuva','tempestade')
+
+#generate_skeleton
+alpha = 0.05
+sensib = 0.01
+Nmin = log(alpha, 1-sensib)-log(length(A), 1-sensib)
+
+skel = startskel(amostra, A, Nmin) #startskel e dependencias: genskel
+sculptskeleton(skel, Nmin) #sculptskel e dependencias: shouldyoucut
+print(skel, 'context','n','transitions')
+
+trans = expandTransitions(skel, A, '-') #expand e dependencias: getMaxContext
+M = transToMatrix(trans, A, '-') #transToMatrix e matrixToTrans, e dep: 
+
+irreductible(M) #tudo rodando ok.
